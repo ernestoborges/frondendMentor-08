b005640bd70a3c37bb26b61b1574ac920d1f4cad
ErnestoBorges <50407385+ernestoborges@users.noreply.github.com>
2022-07-27 18:57:42 -0300

    Update index.js

--- script/index.js
+++ script/index.js
@@ -14,6 +14,16 @@ async function foo() {
 }
 const data = foo();
 console.log(data);
+console.log(data.promise);
+console.log(data.promise.PromiseResult);
+console.log(data.promise.[].PromiseResult);
+console.log(data.Promise.PromiseResult);
+console.log(data.Promise.[].PromiseResult);
+console.log(data[0]);
+console.log(data[1]);
+console.log(data[0].day);
+console.log(data[0].amount);
+
 
 // const data = [
 //     {
